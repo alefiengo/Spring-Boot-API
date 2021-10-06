85d6a4c0115e2a2aa6a6db6497b0b7a30c5ad2d9
José Alejandro Fiengo Vega <jose.fiengo.vega@gmail.com>
2021-10-06 02:37:22 -0400

    Updated test cases

--- src/test/java/com/alefiengo/springboot/api/SpringbootApiApplicationTests.java
+++ src/test/java/com/alefiengo/springboot/api/SpringbootApiApplicationTests.java
@@ -1,13 +1,33 @@
 package com.alefiengo.springboot.api;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 class SpringbootApiApplicationTests {
 
+    Calculator calculator = new Calculator();
+
     @Test
-    void contextLoads() {
+    @DisplayName("Sum of valA and valB")
+    void sumValues() {
+        //given
+        int valA = 2;
+        int valB = 3;
+
+        //when
+        int expected = calculator.sum(valA, valB);
+
+        //then
+        int result = 5;
+        assertThat(expected).isEqualTo(result);
+    }
+
+    static class Calculator {
+        int sum(int val_a, int val_b) {
+            return val_a + val_b;
+        }
     }
 
 }
